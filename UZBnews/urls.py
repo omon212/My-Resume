3716df67b2ca6b07ad3d558e18252736ab037b37
Sharifjon <mominovsharif12@gmail.com>
2023-09-25 18:03:33 +0500

    INdex and news configuration

--- UZBnews/urls.py
+++ UZBnews/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
-    ]
+    path('news.html/', views.news, name='news')
+]
--- UZBnews/views.py
+++ UZBnews/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import KunlikNewuz
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+# def news(request):
+#     return render(request, 'news.html')
+#
+
+def news(request):
+    news_objects = KunlikNewuz.objects.all()  # Fetch all objects, you can use a filter if needed
+
+    return render(request, 'news.html', {'news_objects': news_objects})
 
 # Create your views here.
