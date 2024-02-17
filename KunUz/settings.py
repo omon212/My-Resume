55acf44f0cdec8dd72dcfc0ccc74179f95167a60
Omonullo Raimkulov <omonnuloraimkulov@gmail.com>
2024-02-17 13:28:03 +0500

    fix error

--- KunUz/settings.py
+++ KunUz/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-r7cbxm)74_3c$($xnnuhm%ax1fi1q2-^&p%tzf7knt2s7zy*5^
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost",'omonullo.uz']
-
+# ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
